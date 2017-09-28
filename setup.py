a6654697002e634f4acbfbd640e15ee821bfbc3b
Victor Han <vhan@berkeley.edu>
2017-09-27 19:28:00 -0700

    hopefully cloud works

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 REQUIRED_PACKAGES = ['tensorflow>=1.3.0',
-'gym>=0.1',
+'gym[all]>=0.1',
 'seaborn>=0.1',
 'pandas>=0.1',
 'matplotlib>=0.1',
