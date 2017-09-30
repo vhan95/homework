f2f9134d41af48d408e5bea89839d7d024c1a8f7
Victor Han <vhan@berkeley.edu>
2017-09-30 00:03:47 -0700

    some data recorded for hw3

--- hw3/dqn.py
+++ hw3/dqn.py
@@ -313,6 +313,8 @@ def learn(env,
         if len(episode_rewards) > 100:
             best_mean_episode_reward = max(best_mean_episode_reward, mean_episode_reward)
         if t % LOG_EVERY_N_STEPS == 0 and model_initialized:
+            with open("data.csv", "a") as myfile:
+                myfile.write("%d, %f, %f\n" % (t, mean_episode_reward, best_mean_episode_reward))
             print("Timestep %d" % (t,))
             print("mean reward (100 episodes) %f" % mean_episode_reward)
             print("best mean reward %f" % best_mean_episode_reward)
@@ -320,3 +322,5 @@ def learn(env,
             print("exploration %f" % exploration.value(t))
             print("learning_rate %f" % optimizer_spec.lr_schedule.value(t))
             sys.stdout.flush()
+            
+# data = np.genfromtxt("log.csv", dtype=None, delimiter=', ')
